ce8179221a43447e4a5cba42157369fdaeb81599
Tom Leo <tom@tomleo.com>
2015-08-23 14:28:50 -0400

    Learning Flexbox

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+// This shows a full config file!
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            sass: {
+                files: '*.scss',
+                tasks: ['sass']
+            }
+        },
+        sass: {
+            dev: {
+                files: {
+                    'main.css': 'main.scss'
+                }
+            }
+        },
+        browserSync: {
+            default_options: {
+                bsFiles: {
+                    src: ['*.css', '*.html']
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "./",
+                        directory: true,
+                        index: "index.html"
+                    }
+                }
+            }
+        }
+    });
+
+    // load npm tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    // define default task
+    grunt.registerTask('default', ['browserSync', 'watch']);
+};
